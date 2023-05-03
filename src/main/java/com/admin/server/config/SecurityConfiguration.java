9aaa273c2ee2a38269be1f8604af7259d7bb3ff8
KoalaBear <owen000814@outlook.com>
2023-05-03 22:22:33 +0800

    feat: update

---
+++ src/main/java/com/admin/server/config/SecurityConfiguration.java
@@ -0,0 +1,53 @@
+package com.admin.server.config;
+
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+/**
+ * @author koala
+ * @version 1.0
+ * @date 2023/5/3 21:55
+ * @description
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    private final String adminContextPath;
+
+    public SecurityConfiguration(AdminServerProperties adminServerProperties) {
+        this.adminContextPath = adminServerProperties.getContextPath();
+    }
+
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+        successHandler.setTargetUrlParameter("redirectTo");
+        successHandler.setDefaultTargetUrl(adminContextPath + "/");
+        return http.authorizeHttpRequests((authorizeRequests) -> {
+                    try {
+                        authorizeRequests.requestMatchers(
+                                        adminContextPath + "/instances",
+                                        adminContextPath + "/actuator/**",
+                                        adminContextPath + "/assets/**",
+                                        adminContextPath + "/login"
+                                ).permitAll()
+                                .anyRequest().authenticated()
+                                .and().formLogin().loginPage(adminContextPath + "/login").successHandler(successHandler)
+                                .and().logout().logoutUrl(adminContextPath + "/logout")
+                                .and().httpBasic()
+                                .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+                .build();
+    }
+
+}
